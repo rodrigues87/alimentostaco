681e33f1f94de300f3f593c215be7846b14f3e6a
pedro <pedroh.mix@gmail.com>
2020-07-06 19:56:08 -0300

    Fazendo login com o google

--- usuarios/views.py
+++ usuarios/views.py
@@ -45,7 +45,7 @@ def submit_login_google(request):
 
         try:
             user = User.objects.get(email=email)
-            user = authenticate(username=user.email, password=user.password)
+            authenticate(username=user.email, password=user.password)
             print("Usuario existe e esta autenticado: "+user.password)
 
         except User.DoesNotExist:
