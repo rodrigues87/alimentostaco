d216feefa496ce2c1565285c5563a2b11873c268
pedro.rodrigues <pedroh.mix@gmail.com>
2020-07-09 10:38:53 -0300

    Adicionando template

--- dietas/models.py
+++ dietas/models.py
@@ -9,6 +9,9 @@ class Dieta(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     observacao = models.TextField(max_length=600,blank=True,null=True)
 
+    def __str__(self):
+        return self.nome
+
     class Meta:
         db_table = 'dieta'
 
